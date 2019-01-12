c41d20ebf951517f01a93210c513d6617425ea89
Anna Spysz <anna.spysz@stackery.io>
2019-01-12 10:36:16 -0800

    Added handleSubmit logic

--- src/handleForm/index.js
+++ src/handleForm/index.js
@@ -1,5 +1,34 @@
+const qs = require('qs');
+const AWS = require('aws-sdk');
+
+// write form data to DynamoDB table
+// send welcome SNS
+
 exports.handler = async message => {
   console.log(message);
+  const formData = qs.parse(message.body);
+  console.log(formData);
 
-  return {};
-}
+  const dynamodb = new AWS.DynamoDB();
+  const params = {
+    Item: {
+      'phone': {
+        S: formData.phone
+      },
+      'school': {
+        S: formData.school
+      },
+      'programs': {
+        S: formData.programs // array!
+      }
+    },
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: process.env.TABLE_NAME
+  };
+  await dynamodb.putItem(params).promise();
+
+  return {
+    statusCode: 302,
+    headers: {'Location': 'https://githubpageredirect.com/submitted'}
+  };
+};
