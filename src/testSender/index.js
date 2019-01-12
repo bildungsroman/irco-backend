f173cee5fb5f0b03320cbb9fab8ccc7c30de8bfc
Anna Spysz <anna.spysz@stackery.io>
2019-01-12 13:26:25 -0800

    testing

--- src/testSender/index.js
+++ src/testSender/index.js
@@ -2,6 +2,8 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
+  console.log('==============event==================');
+  console.log(event);
 
 	let receiver = event.receiver;
 	let sender = event.sender;
