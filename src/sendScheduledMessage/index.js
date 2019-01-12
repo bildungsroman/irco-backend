3284566a05e74b086447edd8442bbeeb2b33d566
Anna Spysz <anna.spysz@stackery.io>
2019-01-12 14:45:51 -0800

    schedule function

--- src/sendScheduledMessage/index.js
+++ src/sendScheduledMessage/index.js
@@ -1,7 +1,12 @@
-// read dynamo schedule, send message at right time
+const AWS = require('aws-sdk');
+// Set region
+AWS.config.update({region: 'us-west-2'});
+
+// parse info coming in from Dynamo
+// then send appropriate SNS
 
 exports.handler = async message => {
-  console.log(message);
-
+  const dynamoData = JSON.parse(message.dynamodb);
+  console.log(dynamoData);
   return {};
-}
+};
