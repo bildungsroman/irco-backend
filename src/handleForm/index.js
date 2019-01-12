16fda3b4aec728582b6393226a9a33606d509a43
Anna Spysz <anna.spysz@stackery.io>
2019-01-12 14:17:17 -0800

    trying subscriptions

--- src/handleForm/index.js
+++ src/handleForm/index.js
@@ -28,21 +28,8 @@ exports.handler = async message => {
     ReturnConsumedCapacity: 'TOTAL',
     TableName: process.env.TABLE_NAME
   };
-  console.log(`Writing new user data to table ${process.env.TABLE_NAME}: ${tableParams}`)
+  console.log(`Writing new user data for number ${parsedNumber} to table ${process.env.TABLE_NAME}: ${tableParams}`)
   await dynamodb.putItem(tableParams).promise();
-
-  const sns = new AWS.SNS();
-
-  var subscribeParams = {
-    Protocol: 'SMS', /* required */
-    TopicArn: process.env.TOPIC_ARN, /* required */
-    Endpoint: parsedNumber,
-    ReturnSubscriptionArn: true
-  };
-  await sns.subscribe(subscribeParams, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
 
   // Create publish parameters
   const snsParams = {
