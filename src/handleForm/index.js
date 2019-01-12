07bb6f8dc6bcfa01f884a9b7b03734b3dbf806cc
Anna Spysz <anna.spysz@stackery.io>
2019-01-12 13:43:58 -0800

    adding dynamodb

--- src/handleForm/index.js
+++ src/handleForm/index.js
@@ -1,4 +1,3 @@
-const qs = require('qs');
 const AWS = require('aws-sdk');
 // Set region
 AWS.config.update({region: 'us-west-2'});
@@ -7,34 +6,29 @@ AWS.config.update({region: 'us-west-2'});
 // send welcome SNS
 
 exports.handler = async message => {
-  const sns = new AWS.SNS();
   console.log(message);
-  const formData = message;
-  // const formData = qs.parse(message.body);
-  // console.log(formData);
-  // const number = formData.phone;
   const number = message.phone;
   const parsedNumber = '+1001' + number.replace(/\D/g,''); // convert phone number to E.164 format for SNS
   const welcomeMessage = 'Thank you for signing up for the IRCO Notifier. You will receive text message reminders for your scheduled classes.';
   // initialize dynamodb
-  // const dynamodb = new AWS.DynamoDB();
+  const dynamodb = new AWS.DynamoDB();
 
-  // const tableParams = {
-  //   Item: {
-  //     'phone': {
-  //       S: formData.phone
-  //     },
-  //     'school': {
-  //       S: formData.school
-  //     },
-  //     'programs': {
-  //       S: formData.programs // array!
-  //     }
-  //   },
-  //   ReturnConsumedCapacity: 'TOTAL',
-  //   TableName: process.env.TABLE_NAME
-  // };
-  // await dynamodb.putItem(tableParams).promise();
+  const tableParams = {
+    Item: {
+      'phone': {
+        S: parsedNumber
+      },
+      'school': {
+        S: message.school
+      },
+      'programs': {
+        S: message.programs // array!
+      }
+    },
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: process.env.TABLE_NAME
+  };
+  await dynamodb.putItem(tableParams).promise();
 
   // Create publish parameters
   const snsParams = {
@@ -52,18 +46,6 @@ exports.handler = async message => {
 			},
 		},
   };
-
-  var subscribeParams = {
-    Protocol: 'SMS', /* required */
-    TopicArn: process.env.TOPIC_ARN, /* required */
-    Endpoint: parsedNumber,
-    ReturnSubscriptionArn: true
-  };
-  await sns.subscribe(subscribeParams, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
-  // log to cloudwatch
   console.log(`Sending welcome message to number ${parsedNumber}: ${welcomeMessage}`);
   // Create promise and SNS service object
   const publishTextPromise = new AWS.SNS().publish(snsParams).promise();
