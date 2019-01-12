93b37c49b5f98ea8de29be601f55824b52ee5bf2
Anna Spysz <anna.spysz@stackery.io>
2019-01-12 11:58:46 -0800

    Adding sns logic

--- src/handleForm/index.js
+++ src/handleForm/index.js
@@ -1,5 +1,7 @@
 const qs = require('qs');
 const AWS = require('aws-sdk');
+// Set region
+AWS.config.update({region: 'us-west-2'});
 
 // write form data to DynamoDB table
 // send welcome SNS
@@ -8,9 +10,13 @@ exports.handler = async message => {
   console.log(message);
   const formData = qs.parse(message.body);
   console.log(formData);
-
+  const number = formData.phone;
+  const parsedNumber = '+1001' + number.replace(/\D/g,''); // convert phone number to E.164 format for SNS
+  const welcomeMessage = 'Thank you for signing up for the IRCO Notifier. You will receive text message reminders for your scheduled classes.';
+  // initialize dynamodb
   const dynamodb = new AWS.DynamoDB();
-  const params = {
+
+  const tableParams = {
     Item: {
       'phone': {
         S: formData.phone
@@ -25,10 +31,28 @@ exports.handler = async message => {
     ReturnConsumedCapacity: 'TOTAL',
     TableName: process.env.TABLE_NAME
   };
-  await dynamodb.putItem(params).promise();
+  await dynamodb.putItem(tableParams).promise();
+
+  // Create publish parameters
+  const snsParams = {
+    Message: welcomeMessage, /* required */
+    PhoneNumber: parsedNumber,
+  };
+  console.log(`Sending welcome message to number ${parsedNumber}: ${welcomeMessage}`);
+  // Create promise and SNS service object
+  const publishTextPromise = new AWS.SNS().publish(snsParams).promise();
+
+  // Handle promise's fulfilled/rejected states
+  publishTextPromise.then(
+    function(data) {
+      console.log("MessageID is " + data.MessageId);
+    }).catch(
+      function(err) {
+      console.error(err, err.stack);
+    });
 
   return {
     statusCode: 302,
-    headers: {'Location': 'https://githubpageredirect.com/submitted'}
+    headers: {'Location': 'https://githubpageredirect.com/submitted'} // redirect in react app
   };
 };
--- src/sendScheduledMessage/index.js
+++ src/sendScheduledMessage/index.js
@@ -1,3 +1,5 @@
+// read dynamo schedule, send message at right time
+
 exports.handler = async message => {
   console.log(message);
 
